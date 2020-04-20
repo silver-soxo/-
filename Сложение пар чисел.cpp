b05c5985a4369e47c9857f3defff4b3454bd45f8
silver-soxo <63998579+silver-soxo@users.noreply.github.com>
2020-04-20 11:34:07 +0300

    Add files via upload

---
+++ Сложение пар чисел.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a, b, g = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
